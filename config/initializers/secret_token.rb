f5d5b57d5aa8b44bc5cd1c99915afafc98fd7579
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-08-07 22:33:31 -0700

    generate helpdesk app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = 'a16bd809ae3ce0ac91b26cedf2e2342609146ff6b91c8d1669cd8f2b35a4293d5f496f4689c8cb97f3633a52e85f475cf1ebf993ec7a58903aa6aff2e8c73029'
